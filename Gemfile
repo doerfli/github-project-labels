225c8eaf8dd5643e55ca7f35536377051feb7db4
Marc Doerflinger <marc@doerf.li>
2017-06-18 22:23:56 +0200

    implement update labels

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 gem 'sinatra', '2.0.0'
 gem 'rack', '~> 2.0'
+gem 'rest-client', '~> 2.0'
+gem 'oauth', '~> 0.5.3'
--- web.rb
+++ web.rb
@@ -1,4 +1,14 @@
 require 'sinatra'
+require 'rest-client'
+require 'json'
+require 'oauth'
+
+GITHUB_REPO = 'doerfli/github-project-labels'
+PROJECT_NAME = 'Sample Project'
+
+GITHUB_API_BASE_URL = 'https://api.github.com'
+GITHUB_LABELS_URL = "#{GITHUB_API_BASE_URL}/repos/#{GITHUB_REPO}/labels"
+GITHUB_PROJECTS_URL = "#{GITHUB_API_BASE_URL}/repos/#{GITHUB_REPO}/projects"
 
 before do
   request.body.rewind
@@ -7,5 +17,56 @@ end
 
 post '/card_moved' do
   puts @request_payload
-  "Hello, world"
+  return 'Card is note - no labels to change' if @request_payload['project_card']['note'] != nil
+  return 'Action was not move' if @request_payload['action'] != 'moved'
+
+  labels = {}
+  response = RestClient.get(GITHUB_LABELS_URL, :Authorization => "token #{ENV['ACCESS_TOKEN']}")
+  labels_json = JSON.parse(response)
+  labels_json.each { |lab|
+    labels[lab['name']] = {
+      #name: lab['name'],
+      id: lab['id'],
+      # url: lab['url']
+    }
+  }
+  puts labels
+
+  projects = {}
+  response = RestClient.get(GITHUB_PROJECTS_URL, :accept => 'application/vnd.github.inertia-preview+json', :Authorization => "token #{ENV['ACCESS_TOKEN']}")
+  projects_json = JSON.parse(response)
+  projects_json.each { |prj|
+    # puts prj
+    projects[prj['name']] = prj['id']
+  }
+
+  columns = {}
+  columns_url = "#{GITHUB_API_BASE_URL}/projects/#{projects[PROJECT_NAME]}/columns"
+  response = RestClient.get(columns_url, :accept => 'application/vnd.github.inertia-preview+json', :Authorization => "token #{ENV['ACCESS_TOKEN']}")
+  columns_json = JSON.parse(response)
+  columns_json.each { |col|
+    # puts col
+    columns[col['id']] = col['name']
+  }
+  puts columns
+
+  issue_id = @request_payload['project_card']['id']
+  column_id_from = @request_payload['changes']['column_id']['from']
+  column_id_to = @request_payload['project_card']['column_id']
+  label_remove = columns[column_id_from]
+  label_add = columns[column_id_to]
+
+  puts "issue #{issue_id}  labels -#{label_remove} +#{label_add}"
+
+  label_remove_url = "#{GITHUB_API_BASE_URL}/repos/#{GITHUB_REPO}/issues/#{issue_id}/labels/#{label_remove}"
+  response = RestClient.delete(label_remove_url, :Authorization => "token #{ENV['ACCESS_TOKEN']}")
+
+  puts "label removed"
+
+  label_add_url = "#{GITHUB_API_BASE_URL}/repos/#{GITHUB_REPO}/issues/#{issue_id}/labels"
+  response = RestClient.post(label_add_url, [label_add], :accept => 'application/vnd.github.inertia-preview+json', :Authorization => "token #{ENV['ACCESS_TOKEN']}")
+
+  puts "label added"
+
+  "issue #{issue_id}  labels -#{label_remove} +#{label_add}"
 end
