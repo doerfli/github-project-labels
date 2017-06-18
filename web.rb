bd230d9afbddfd58672d11e24b476c7daab7e7a2
Marc Doerflinger <marc@doerf.li>
2017-06-18 22:52:43 +0200

    remove debug output

--- web.rb
+++ web.rb
@@ -7,8 +7,6 @@ require 'oauth'
 GITHUB_REPO = 'doerfli/github-project-labels'
 # the name of the project to track
 PROJECT_NAME = 'Sample Project'
-# the secret to verify the source of the request
-SECRET = ''
 
 GITHUB_API_BASE_URL = 'https://api.github.com'
 GITHUB_LABELS_URL = "#{GITHUB_API_BASE_URL}/repos/#{GITHUB_REPO}/labels"
@@ -20,9 +18,9 @@ before do
 end
 
 post '/card_moved' do
-  puts @request_payload
-  return 'Card is note - no labels to change' if @request_payload['project_card']['note'] != nil
-  return 'Action was not move' if @request_payload['action'] != 'moved'
+  #puts @request_payload
+  puts 'card is note' && return 'Card is note - no labels to change' if @request_payload['project_card']['note'] != nil
+  puts 'action was not move' && return 'Action was not move' if @request_payload['action'] != 'moved'
 
   labels = {}
   response = RestClient.get(GITHUB_LABELS_URL, :Authorization => "token #{ENV['ACCESS_TOKEN']}")
@@ -34,7 +32,7 @@ post '/card_moved' do
       # url: lab['url']
     }
   }
-  puts labels
+  #puts labels
 
   projects = {}
   response = RestClient.get(GITHUB_PROJECTS_URL, :accept => 'application/vnd.github.inertia-preview+json', :Authorization => "token #{ENV['ACCESS_TOKEN']}")
@@ -52,7 +50,7 @@ post '/card_moved' do
     # puts col
     columns[col['id']] = col['name']
   }
-  puts columns
+  #puts columns
 
   issue_id = @request_payload['project_card']['id']
   column_id_from = @request_payload['changes']['column_id']['from']
@@ -64,13 +62,13 @@ post '/card_moved' do
   card_content_url = @request_payload['project_card']['content_url']
 
   label_remove_url = "#{card_content_url}/labels/#{URI.escape(label_remove)}"
-  puts label_remove_url
+  #puts label_remove_url
   response = RestClient.delete(label_remove_url, :Authorization => "token #{ENV['ACCESS_TOKEN']}")
 
   puts "label removed: #{label_remove}"
 
   label_add_url = "#{card_content_url}/labels"
-  puts label_add_url
+  #puts label_add_url
   response = RestClient.post(label_add_url, [label_add].to_json, :content_type => :json, :accept => 'application/vnd.github.inertia-preview+json', :Authorization => "token #{ENV['ACCESS_TOKEN']}")
 
   puts "label added: #{label_add}"
