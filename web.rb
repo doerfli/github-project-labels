ff8df33b445ac64eec06845063a34ac9857af7e2
Marc Doerflinger <marc@doerf.li>
2017-06-19 21:21:42 +0200

    support initial add of card to project

--- web.rb
+++ web.rb
@@ -20,8 +20,11 @@ end
 post '/card_moved' do
   #puts @request_payload
   return 'Card is note - no labels to change' if @request_payload['project_card']['note'] != nil
-  return 'Action was not move' if @request_payload['action'] != 'moved'
+  return 'Action was not move' if ! ['created', 'moved'].include?(@request_payload['action'])
 
+  card_added = @request_payload['action'].eql? 'created'
+
+  # get all labels
   labels = {}
   response = RestClient.get(GITHUB_LABELS_URL, :Authorization => "token #{ENV['ACCESS_TOKEN']}")
   labels_json = JSON.parse(response)
@@ -34,6 +37,7 @@ post '/card_moved' do
   }
   #puts labels
 
+  # get all projects in repository
   projects = {}
   response = RestClient.get(GITHUB_PROJECTS_URL, :accept => 'application/vnd.github.inertia-preview+json', :Authorization => "token #{ENV['ACCESS_TOKEN']}")
   projects_json = JSON.parse(response)
@@ -42,6 +46,7 @@ post '/card_moved' do
     projects[prj['name']] = prj['id']
   }
 
+  # get all columns in tracked repositories
   columns = {}
   PROJECT_NAMES.each{ |prj_name|
     columns_url = "#{GITHUB_API_BASE_URL}/projects/#{projects[prj_name]}/columns"
@@ -52,25 +57,32 @@ post '/card_moved' do
       columns[col['id']] = col['name']
     }
   }
-  puts columns
+  #puts columns
 
+  # extract issues, labels and columns
   issue_id = @request_payload['project_card']['id']
   column_id_from = @request_payload['changes']['column_id']['from']
   column_id_to = @request_payload['project_card']['column_id']
-  label_remove = columns[column_id_from]
-  return 'from column not found' if label_remove.nil?
+  unless card_added
+    label_remove = columns[column_id_from]
+    return 'from column not found' if label_remove.nil?
+  end
   label_add = columns[column_id_to]
   return 'to column not found' if label_add.nil?
 
   puts "issue #{issue_id} labels -#{label_remove} +#{label_add}"
   card_content_url = @request_payload['project_card']['content_url']
 
-  label_remove_url = "#{card_content_url}/labels/#{URI.escape(label_remove)}"
-  #puts label_remove_url
-  response = RestClient.delete(label_remove_url, :Authorization => "token #{ENV['ACCESS_TOKEN']}")
+  # remove old label
+  unless card_added
+    label_remove_url = "#{card_content_url}/labels/#{URI.escape(label_remove)}"
+    #puts label_remove_url
+    response = RestClient.delete(label_remove_url, :Authorization => "token #{ENV['ACCESS_TOKEN']}")
 
-  puts "label removed: #{label_remove}"
+    puts "label removed: #{label_remove}"
+  end
 
+  # add new label
   label_add_url = "#{card_content_url}/labels"
   #puts label_add_url
   response = RestClient.post(label_add_url, [label_add].to_json, :content_type => :json, :accept => 'application/vnd.github.inertia-preview+json', :Authorization => "token #{ENV['ACCESS_TOKEN']}")
